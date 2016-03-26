ff460db435c78fe9ed380bd4a848097582b6f4ea
Afuad HOSSAIN <ahossa6@trusty>
2016-03-26 01:06:41 -0400

    v5 parse.c

--- parse.c
+++ parse.c
@@ -5,55 +5,57 @@
 #include "syntax.h"
 #define BOOLEAN int
 
-//intializing the buffer and backup buffer to be used by the following functions
+//intializing the buffer, pointers to be used by the following functions
 char buffer [1000];
-char buffer_backup [1000];
+char tokenBuffer[32];
+char *p;
+char *q;
 
 void initBuffer(char *inputLine){
 // intializing a buffer where we will store the string passed in the parameter inputLine
-	int i;
-	for ( i=0; i<=1000; i++){
-		buffer[i]=inputLine[i];
-		// subsequently storing a backup of that buffer
-		buffer_backup[i]=buffer[i];
-	}
+	strcpy(buffer,inputLine);
+	//have the pointers point at the first position of buffer
+	p=&buffer[0];
+	q=&buffer[0];
+	//take note of initial buffer for the sake of rewinding
 }
 BOOLEAN hasNextToken(){
 //this method determines whether there exists more space in the buffer
-	if(buffer[0]== '\0'){
+	if(*p== '\0'){
 		return 0;
 	}
 	else{
 		return 1;
 	}
 }
-
 char *nextToken(){
 //this function removes the first token from the buffer, the removed token is returned
-	//tokenizing the input line and saving first token into the variable token
-	char *token;
-	token=strtok(buffer, " ");
 	
-	if(token!=NULL){
-		int i=0;
-		//shifting buffer by the size of token, so it points at the next token
-		while(buffer[i]!='\0'){
-			buffer[i]=buffer[i+strlen(token)+1];
-			i++;
-		}
-		return token;
+	//if buffer is empty, returns NULL	
+	if(*p=='\0'){
+ 		return NULL;
 	}
-	else{
-		return NULL;
+	//q moves across the buffer until it hits a space or null terminator
+	while(*q!=' ' && *q!='\0') {
+		q++;
 	}
+	int i=0;
+	//we save the trail between p and q into the tokenBuffer, which holds one token
+	//moving p forward until it's one space behind q
+	while(p<q) {
+		tokenBuffer[i] = *p;
+		i++;
+		p++;
+	}
+	tokenBuffer[i]='\0';
+	//we move p and q to the next token if and only they both land on a space
+	if(p!='\0'){
+		 p++;
+	}
+	q=p;
+	return tokenBuffer;
 }
 void myrewind(){
 // reinitializes the private buffer with the string originally provided by initBuffer() function call
-	int i;
-	// using the backup we had initially saved, we reintialize the buffer
-	for( i-0; i<1000; i++){
-		buffer[i]=buffer_backup[i];
-	}
+	p = &buffer[0];
 }
-
-
