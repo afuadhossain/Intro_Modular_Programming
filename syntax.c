4e45f205e1f20232cb5d23bcdc8a2632a902e18f
Afuad HOSSAIN <ahossa6@trusty>
2016-03-26 01:07:08 -0400

    v5 syntax.c

--- syntax.c
+++ syntax.c
@@ -7,13 +7,8 @@
 int isValidCommand(char *token){
 // A robot command is atomic. It is a single word that directs the robot to do a specific self-contained task. Legal commands are: TAKEASTEP, LEFT, RIGHT, PICKUP, DROP, DETECTMARKER, TURNON, and TURNOFF. These commands are not case sensitive, so Walk, walk, walK, and WALK are all valid
 	
-	//we remove the comma from the end of the token
-	if(token[strlen(token)-1] == ','){
-		token[strlen(token)-1] == '\0';
-	}
 	//verifying that commands are valid, else return 0;
-	if(strcasecmp(token,"TAKEASTEP")==0 || strcasecmp(token,"LEFT")==0 || strcasecmp(token, 	"RIGHT")==0 || strcasecmp(token, "PICKUP")==0 || strcasecmp(token, "DROP")==0 || 		strcasecmp(token, "DETECTMARKER")==0 || strcasecmp(token, "TURNON")==0 || 
-	strcasecmp(token, "TURNOFF")==0){
+	if(strcasecmp(token,"TAKEASTEP")==0 || strcasecmp(token,"LEFT")==0 || strcasecmp	(token,"RIGHT")==0 || strcasecmp(token, "PICKUP")==0 || strcasecmp(token, "DROP")==0 || strcasecmp(token, 		"DETECTMARKER")==0 || strcasecmp(token, "TURNON")==0 ||	strcasecmp(token, "TURNOFF")==0){
 		return 1;
 	}
 	else{
@@ -35,71 +30,80 @@ returns false.
 SAY “message”
 ◦ The robot speaks the words contained in the string “message”.
 */
-	
+	int i;
+	for(i=0; i<strlen(expression); i++) {
+		if(expression[i]==',') {
+			expression[i]=' ';
+		}
+	}
 	initBuffer(expression);
 	char *token=nextToken();
-	//in the next set of if statements, we are checking to see if the first token is either 	REPEAT, WHILE, or SAY, else we return 0.
+	//in the next set of if statements, we are checking to see if the first token is either REPEAT, WHILE, or SAY, else we return 0.
 
 	
-	//Checking to see that the expression follows the structure: REPEAT n TIMES comma-separated-  		list-of-commands END
+	//Checking to see that the expression follows the structure: REPEAT n TIMES comma-separated-list-of-commands END
 	
 	if (strcasecmp(token, "REPEAT")==0){
 		token=nextToken();
+		if(token==NULL) return 0; //////////////////////////////////////
 		if(atoi(token)<=0){
 			return 0;
 		}
 		token=nextToken();
+		if(token==NULL) return 0; //////////////////////////////////
 		if(strcasecmp(token, "TIMES")!=0){
 			return 0;
 		}
 		token=nextToken();
+		if(token==NULL) return 0; ///////////////////////////////////
 		while(isValidCommand(token)==1){
 			token=nextToken();
+			if(token==NULL) return 0;////////////////////////////
 		}
 		if(strcasecmp(token, "END")!=0){
 			return 0;
 		}
 		token=nextToken();
-		if(token!=NULL){
-			return 0;
-		}
-		return 1;
+		if(token==NULL) return 1;
+		
 	}
-	//Checking to see that the expression follows the structure: WHILE NOT c DO comma-separated 		list-of-commands END
+	//Checking to see that the expression follows the structure: WHILE NOT c DO comma-separated list-of-commands END
 
 	if (strcasecmp(token, "WHILE")==0){
+		token=nextToken();
+		if(token==NULL) return 0; //////////////////////////////
 		if(strcasecmp(token, "NOT")!=0){
 			return 0;
 		}
 		token=nextToken();
+		if(token==NULL) return 0; //////////////////////////////
 		if(strcasecmp(token, "DETECTMARKER")!=0){
 			return 0;
 		}
 		token=nextToken();
+		if(token==NULL) return 0; //////////////////////////////
 		if(strcasecmp(token, "DO")!=0){
 			return 0;
 		}
 		token=nextToken();
+		if(token==NULL) return 0; //////////////////////////////
 		while(isValidCommand(token)==1){
 			token=nextToken();
+			if(token==NULL) return 0; //////////////////////////////
 		}
 		if(strcasecmp(token, "END")!=0){
 			return 0;
 		}
 		token=nextToken();
-		if(token!=NULL){
-			return 0;
-		}
-		return 1;
+		if(token==NULL) return 1;
 	}
 	//Checking to see that the expression follows the structure: SAY "message"
 	if (strcasecmp(token, "SAY")==0){
 		token=nextToken();
+		if(token==NULL) return 0; //////////////////////////////
 		if(token!=NULL){
 			return 1;	
 		}
 	}
 	return 0;
 }
-void main(void)
-{}
