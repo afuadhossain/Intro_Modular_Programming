9e99dd9d7aa6d4e9e842c99a770609d45b20e425
Afuad HOSSAIN <ahossa6@trusty>
2016-03-25 22:57:22 -0400

    v2 syntax

--- syntax.c
+++ syntax.c
@@ -7,25 +7,30 @@
 int isValidCommand(char *token){
 // A robot command is atomic. It is a single word that directs the robot to do a specific self-contained task. Legal commands are: TAKEASTEP, LEFT, RIGHT, PICKUP, DROP, DETECTMARKER, TURNON, and TURNOFF. These commands are not case sensitive, so Walk, walk, walK, and WALK are all valid
 	
-	//since we don't care for case-sensitivity, we convert everything to upper case for simplicity
-	int i=0;
-	while(token[i]!='\0'){
-		token[i]=toupper(token[i]);
-		i++
-	}
 	//we remove the comma from the end of the token
 	if(token(strlen(token)-1) == ","){
 		token(strlen(token)-1) == '\0';
 	}
 	//verifying that commands are valid, else return 0;
-	if(token="TAKEASTEP" ||token="LEFT" || token="RIGHT" || token="PICKUP" || token="DROP" || 		token="DETECTMARKER" || token="TURNON" || token="TURNOFF"){
+	if(strcasecmp(token,"TAKEASTEP")==0 || strcasecmp(token,"LEFT")==0 || strcasecmp(token, 	"RIGHT")==0 || strcasecmp(token, "PICKUP")==0 || strcasecmp(token, "DROP")==0 || 		strcasecmp(token, "DETECTMARKER")==0 || strcasecmp(token, "TURNON")==0 || 
+	strcasecmp(token="TURNOFF")==0){
 		return 1;
 	}
 	else{
 		return 0;
-	}
-	
+	}	
 }
 int isValidExpression(char *expression){
+
+
+
+
+
+
+
+
+
+
+
 
 }
