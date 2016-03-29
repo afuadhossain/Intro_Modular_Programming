d2cb5f6015f0413efce65dfaac0dfb7913df713b
Afuad HOSSAIN <ahossa6@trusty>
2016-03-29 18:39:31 -0400

    final version main.c

--- main.c
+++ main.c
@@ -5,18 +5,18 @@
 #include "parse.h"
 
 void main(int argc,char *argv[])
-{	
-	
+{
+
 	//will throw an error statement if you have entered more than one text file
 	if(argc==1){
-		printf("You have not entered a txt file, please enter one txt file following the format: ./a.out NAME.txt\n");	
+		printf("You have not entered a txt file, please enter one txt file following the format: ./a.out NAME.txt\n");
 	}
 	//will throw an error statement if you have entered no text file
 	if(argc>2){
-		printf("You have entered more than one txt file, please enter one txt file following the format: ./a.out NAME.txt\n");	
+		printf("You have entered more than one txt file, please enter one txt file following the format: ./a.out NAME.txt\n");
 	}
-	
-	//intializing a file pointer that will point to the designated file	
+
+	//intializing a file pointer that will point to the designated file
 	FILE *file_ptr;
 	char line[300];
 	//opening the designated file
@@ -33,17 +33,17 @@ void main(int argc,char *argv[])
 	while (!feof(file_ptr)){
 		//this get  rids of the new line that is also taken when using fget for a line
 		line[strcspn(line,"\n")]=0;
-		//No spaces, command
+		//No spaces, it's a command
 		if(strchr(line,' ')==NULL) {
 			if (isValidCommand(line)==0){
-				printf("\nERROR! You messed up on Line: %d\nInvalid command: ***%s\nAcceptable commands: TAKEASTEP,LEFT,RIGHT,PICKUP,DROP,DETECTMARKER,TURNON,TURNOFF\n", linenumber, line);
-				
+				printf("\nERROR! You messed up on Line: %d\nInvalid command: ***%s\nAcceptable commands:TAKEASTEP,LEFT,RIGHT,PICKUP,DROP,DETECTMARKER,TURNON,TURNOFF\n", linenumber, line);
+
 			}
-		} 
-		//Has spaces, expression
+		}
+		//Has spaces, it's an expression
 		else {
 			if (isValidExpression(line)==0){
-				printf("\nERROR! You messed up on line: %d\nInvalid expresison: %s\n", linenumber, line);
+				printf("\nERROR! You messed up on line: %d\nInvalid expression: %s\n", linenumber, line);
 			}
 		}
 		linenumber++;
@@ -51,4 +51,3 @@ void main(int argc,char *argv[])
 	}
 	fclose(file_ptr);
 }
-
